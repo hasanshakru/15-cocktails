499c1b8185a1f5b43b6c3a2deb22c3661470607b
hasanshakru <hasanshakru@gmail.com>
2022-10-07 19:46:56 +0530

    New menu repo

---
+++ setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel
+        voluptatibus aspernatur iure aut impedit architecto iusto velit
+        molestiae nulla amet, expedita ex, quia repellat. Ut vitae quia sit
+        quaerat, odit quasi consequuntur soluta, dignissimos accusamus
+        blanditiis cum quam mollitia similique?
+      </p>
+    </section>
+  );
+};
+
+export default About;
